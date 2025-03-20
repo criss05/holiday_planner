323fcd76f9cf22d7942470af5ec2125aba598ac8
Barnut Cristiana <119601150+criss05@users.noreply.github.com>
2025-03-20 22:56:43 +0200

    Change the color for the menu buttons

--- src/components/UI/FilterButton.js
+++ src/components/UI/FilterButton.js
@@ -2,9 +2,9 @@ export default function FilterButton({ label, onClick }) {
     return (
       <button
         onClick={onClick}
-        className="bg-blue-500 text-white px-4 py-2 rounded-md w-full mb-4 text-center"
+        className="bg-[#68ccf4] px-4 py-2 rounded-md w-full mb-4 text-center"
       >
-        {label}
+        <strong>{label}</strong>
       </button>
     );
   }
