36ff79f1b0c5c680e785679f4e0b0f21c53b6aee
Barnut Cristiana <119601150+criss05@users.noreply.github.com>
2025-03-20 22:54:25 +0200

    ChangeSomeColorsAndFonts

--- src/components/Holidays/DeletePopUp.js
+++ src/components/Holidays/DeletePopUp.js
@@ -5,24 +5,28 @@ export default function DeletePopup({ isVisible, onClose, onDelete, holidayName 
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-[rgba(0,0,0,0.5)] z-50">
-      <div className="bg-[#D3E7FF] p-6 rounded-lg shadow-lg w-1/3">
+      <div className="bg-[#F2F2F2] p-6 rounded-lg shadow-lg w-1/3">
         <h3 className="text-lg font-bold mb-4">Are you sure you want to delete this holiday?</h3>
         <p className="mb-4"><strong>{holidayName}</strong></p>
         <div className="flex gap-4 justify-center">
           <button
-            className="bg-[#D3E7FF] px-6 py-2 rounded-md"
+            className="bg-[#73DAFF] px-6 py-2 rounded-md"
             onClick={() => {
               onDelete(holidayName);
               onClose();
             }}
           >
+            <strong>
             Yes
+            </strong>
           </button>
           <button
-            className="bg-[#D3E7FF] px-6 py-2 rounded-md"
+            className="bg-[#73DAFF] px-6 py-2 rounded-md"
             onClick={onClose}
           >
+           <strong>
             No
+            </strong>
           </button>
         </div>
       </div>
