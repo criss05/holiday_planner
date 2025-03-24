e1832391dfb60c3e0b7b9446ac6887f55a496ffb
Barnut Cristiana <119601150+criss05@users.noreply.github.com>
2025-03-24 15:56:40 +0200

    Add the header in the Add Page

---
+++ src/pages/AddPage.js
@@ -0,0 +1,9 @@
+import HeaderAdd from "@/components/UI/AddPage/HeaderAdd"
+
+export default function AddPage(){
+    return (
+        <div className="min-h-screen">
+            <HeaderAdd />
+        </div>
+    );
+}
