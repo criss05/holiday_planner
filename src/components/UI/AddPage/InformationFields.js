f3e7ab230bd5f7a74c8c23b4d58d6fd83daf814d
Barnut Cristiana <119601150+criss05@users.noreply.github.com>
2025-03-24 21:00:26 +0200

    Create the input boxes based on the text

--- src/components/UI/AddPage/InformationFields.js
+++ src/components/UI/AddPage/InformationFields.js
@@ -1,12 +1,31 @@
+import Calendar from "./Calendar";
 import InputBox from "./InputBox";
+import TransportOptions from "./TransportOptions";
+import TransportPrice from "./TransportPriceBox";
 
 export default function InformationFields({ text, icon }) {
     return (
-        <div className="grid grid-cols-2 gap-6 p-3">
-            <label className="text-gray-800 font-semibold text-3xl">{text}:</label>
+        <div className="grid grid-cols-2 gap-6 pb-10">
+            <label className="text-gray-800 font-semibold text-3xl pr-10">{text}:</label>
             <div className="flex items-center bg-blue-100 border border-blue-300 px-3 py-2">
                 {icon && <span className="mr-2 text-2xl">{icon}</span>}
-                <InputBox text={text}/>
+                {text.toLowerCase().includes("name") || text.toLowerCase().includes("where") ? (
+                    <InputBox />
+                ) : text.toLowerCase().includes("transport") ? (
+                    <div className="grid grid-cols-2">
+                        <div className="pl-3">
+                            <TransportOptions />
+                        </div>
+                        <div className="flex align-center items-center">
+                            <TransportPrice />
+                        </div>
+                    </div>
+                ) : text.toLowerCase().includes("when") ? (
+                    <Calendar />
+                ) : (
+                    <InputBox />
+                )
+                }
             </div>
         </div>
     );
