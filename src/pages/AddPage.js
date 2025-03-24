96978e118e8a70e8e21a55ff794237a217be8bc9
Barnut Cristiana <119601150+criss05@users.noreply.github.com>
2025-03-24 17:07:05 +0200

    Use the fields created before for each category needed

--- src/pages/AddPage.js
+++ src/pages/AddPage.js
@@ -1,9 +1,20 @@
 import HeaderAdd from "@/components/UI/AddPage/HeaderAdd"
+import InformationFields from "@/components/UI/AddPage/InformationFields";
+import { FaMapMarkedAlt } from "react-icons/fa";
 
-export default function AddPage(){
+export default function AddPage() {
     return (
         <div className="min-h-screen">
             <HeaderAdd />
+            <div className="w-3/5 flex flex-col justify-between p-15">
+                <InformationFields text="Holiday Name" />
+                <InformationFields text="Where would you like to travel?" icon={<FaMapMarkedAlt />} />
+                <InformationFields text="What type of transport would you like to use?" />
+                <InformationFields text="When would you like to travel?" />
+                <InformationFields text="What is the type of accommodation you would prefer?" />
+            </div>
+
+
         </div>
     );
 }
