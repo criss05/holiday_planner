4a9413fbfc8097e26859efb68b9e6f2d3608ca16
Barnut Cristiana <119601150+criss05@users.noreply.github.com>
2025-03-24 15:56:26 +0200

    Connect the add button to the AddPage

--- src/components/UI/HomePage/AddButton.js
+++ src/components/UI/HomePage/AddButton.js
@@ -1,7 +1,14 @@
+import Link from 'next/link';
 import { FaPlus } from 'react-icons/fa';
+import AddPage from "../../../pages/AddPage";
+
 
 export default function AddButton() {
-    return (<button className="fixed bottom-6 right-6 bg-[#73DAFF] border-2 p-4 rounded-full shadow-lg">
-        <FaPlus className="text-2xl" />
-    </button>);
+    return (
+        <Link href="/AddPage">
+            <button className="fixed bottom-6 right-6 bg-[#73DAFF] border-2 p-4 rounded-full shadow-lg">
+                <FaPlus className="text-2xl" />
+            </button>
+        </Link>
+    );
 }
