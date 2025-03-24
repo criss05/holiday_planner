17c8ab600cf2feb9f601b5ee73fac433c4285f51
Barnut Cristiana <119601150+criss05@users.noreply.github.com>
2025-03-24 17:05:49 +0200

    Create an input box for writing the holiday information

---
+++ src/components/UI/AddPage/InputBox.js
@@ -0,0 +1,8 @@
+export default function InputBox({ text}) {
+    return (
+        <input
+            type="text"
+            className="bg-transparent flex-1 outline-none text-gray-700 placeholder-gray-500"
+        />
+    );
+}
