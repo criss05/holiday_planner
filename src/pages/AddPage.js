ce00bac378e45ef94a54f9ea027dd235425bdf3e
Barnut Cristiana <119601150+criss05@users.noreply.github.com>
2025-03-25 10:41:19 +0200

    Adding the right side bar for TO DO list

--- src/pages/AddPage.js
+++ src/pages/AddPage.js
@@ -9,18 +9,21 @@ export default function AddPage() {
     return (
         <div className="min-h-screen">
             <HeaderAdd />
-            <div className="w-3/5 flex flex-col justify-between p-20">
-                <InformationFields text="Holiday Name" />
-                <InformationFields text="Where would you like to travel?" icon={<FaMapMarkedAlt />} />
-                <InformationFields text="What type of transport would you like to use?" />
-                <InformationFields text="When would you like to travel?" />
-                <InformationFields text="What is the type of accommodation you would prefer?" />
-                <div className="flex justify-left gap-20">
-                    <DoneButton />
-                    <CancelButton />
+            <div className="flex flex-row justify-between h-screen">
+                <div className="w-3/5 flex flex-col justify-between p-20">
+                    <InformationFields text="Holiday Name" />
+                    <InformationFields text="Where would you like to travel?" icon={<FaMapMarkedAlt />} />
+                    <InformationFields text="What type of transport would you like to use?" />
+                    <InformationFields text="When would you like to travel?" />
+                    <InformationFields text="What is the type of accommodation you would prefer?" />
+                    <div className="flex justify-left gap-20">
+                        <DoneButton />
+                        <CancelButton />
+                    </div>
+                </div>
+                <div className="bg-[#C3E9F8] w-xl h-full">
                 </div>
             </div>
-
 
         </div>
     );
