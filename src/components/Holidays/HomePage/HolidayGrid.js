4c454d4b36bcee04a1082fe13b18167b17562ff3
Barnut Cristiana <119601150+criss05@users.noreply.github.com>
2025-03-26 23:55:37 +0200

    Add edit functionality to HolidayGrid component

--- src/components/Holidays/HomePage/HolidayGrid.js
+++ src/components/Holidays/HomePage/HolidayGrid.js
@@ -1,11 +1,10 @@
 import HolidayCard from "./HolidayCard";
 
-
-export default function HolidayGrid({ holidays, onDelete}) {
+export default function HolidayGrid({ holidays, onDelete, onEdit }) {
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-20 px-25 py-7">
             {holidays.map((holiday) => (
-                <HolidayCard holiday={holiday} onDelete={onDelete}/>
+                <HolidayCard key={holiday.name} holiday={holiday} onDelete={onDelete} onEdit={onEdit}/>
             ))}
         </div>
     );
