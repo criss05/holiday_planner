f03b9f2c196460c41651eaa6c3bdb224f594226b
Barnut Cristiana <119601150+criss05@users.noreply.github.com>
2025-03-26 23:55:32 +0200

    Add edit functionality to HolidayCard component

--- src/components/Holidays/HomePage/HolidayCard.js
+++ src/components/Holidays/HomePage/HolidayCard.js
@@ -1,8 +1,7 @@
 import { FaEdit, FaTrash, FaInfoCircle } from "react-icons/fa";
-import DetailsPage from "@/pages/DetailsPage";
 import Link from "next/link";
 
-export default function HolidayCard({ holiday, onDelete }) {
+export default function HolidayCard({ holiday, onDelete, onEdit }) {
     return (
         <div key={holiday.name} className="bg-[#D3E7FF] shadow-lg flex flex-col justify-between h-64">
             <h2 className="text-xl font-bold bg-[#A7CFFF] text-center py-3">{holiday.name}</h2>
@@ -13,12 +12,10 @@ export default function HolidayCard({ holiday, onDelete }) {
                 <p><strong>Transport: </strong>{holiday.transport}</p>
             </div>
             <div className="flex justify-end gap-4 px-4 pb-3">
-                <div className="cursor-pointer bg-[#73DAFF] p-3 rounded-md">
+                <div className="cursor-pointer bg-[#73DAFF] p-3 rounded-md" onClick={() => onEdit(holiday)}>
                     <FaEdit className=" text-lg" />
                 </div>
-                <div className="cursor-pointer bg-[#73DAFF] p-3 rounded-md"
-                    onClick={() => onDelete(holiday.name)}
-                >
+                <div className="cursor-pointer bg-[#73DAFF] p-3 rounded-md" onClick={() => onDelete(holiday.name)}>
                     <FaTrash className="text-lg" />
                 </div>
                 <Link href="/DetailsPage">
