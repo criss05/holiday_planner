dc69e5a1ed710fa1d13185362bc3a2de4d96f5dd
Barnut Cristiana <119601150+criss05@users.noreply.github.com>
2025-03-27 01:14:13 +0200

    Add InputBox component for text input functionality

---
+++ src/components/UI/DetailsPage/InputBox.js
@@ -0,0 +1,11 @@
+export default function InputBox({value, disabled, name }) {
+    return (
+        <input
+            type="text"
+            className="bg-transparent flex-1 outline-none text-gray-700 placeholder-gray-500"
+            value = {value}
+            disabled={disabled}
+            name={name}
+        />
+    );
+}
