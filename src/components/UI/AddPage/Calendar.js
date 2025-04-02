504158f7e3b3635eb2349c612e70745b3b513126
Barnut Cristiana <119601150+criss05@users.noreply.github.com>
2025-04-02 18:53:34 +0300

    change the min date for the edit and add

--- src/components/UI/AddPage/Calendar.js
+++ src/components/UI/AddPage/Calendar.js
@@ -3,7 +3,7 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 
-export default function Calendar({ startDate, endDate, onChange }) {
+export default function Calendar({ startDate, endDate, onChange, minDate }) {
     const formatDate = (date) =>
         date ? date.toLocaleDateString("en-US", { year: "numeric", month: "long", day: "numeric" }) : "";
 
@@ -14,7 +14,7 @@ export default function Calendar({ startDate, endDate, onChange }) {
                 <DatePicker
                     selected={startDate ? new Date(startDate) : null}
                     onChange={(date) => onChange({ startDate: formatDate(date), endDate })}
-                    minDate={new Date()}
+                    minDate={minDate}
                     dateFormat="MMMM d, yyyy"
                     className="p-2 border-2 border-[#A7CFFF] rounded-md w-full"
                 />
@@ -23,7 +23,7 @@ export default function Calendar({ startDate, endDate, onChange }) {
                 <label className="block text-gray-700 font-semibold mb-2">End Date: </label>
                 <DatePicker
                     selected={endDate ? new Date(endDate) : null}
-                    minDate={startDate ? new Date(startDate) : new Date()}
+                    minDate={startDate ? new Date(startDate) : new Date()}s
                     onChange={(date) => onChange({ startDate, endDate: formatDate(date) })}
                     dateFormat="MMMM d, yyyy"
                     className="p-2 border-2 border-[#A7CFFF] rounded-md w-full"
