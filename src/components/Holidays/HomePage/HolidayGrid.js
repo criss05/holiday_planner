bb9a941302f9acbc97432be9316e63acf54ae5dc
Barnut Cristiana <119601150+criss05@users.noreply.github.com>
2025-04-03 00:38:43 +0300

    Add data-testid attribute to HolidayGrid component for improved testing

--- src/components/Holidays/HomePage/HolidayGrid.js
+++ src/components/Holidays/HomePage/HolidayGrid.js
@@ -2,9 +2,15 @@ import HolidayCard from "./HolidayCard";
 
 export default function HolidayGrid({ holidays, onDelete, onEdit, onView }) {
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-20 px-25 py-7">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-20 px-25 py-7" 
+        data-testid="holiday-grid">
             {holidays.map((holiday) => (
-                <HolidayCard key={holiday.id} holiday={holiday} onDelete={onDelete} onEdit={onEdit} onView={onView}/>
+                <HolidayCard key={holiday.id} 
+                holiday={holiday} 
+                onDelete={onDelete} 
+                onEdit={onEdit} 
+                onView={onView}
+                />
             ))}
         </div>
     );
