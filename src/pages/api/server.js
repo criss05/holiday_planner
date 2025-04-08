189d616a9969318d340918d96786e65c8c2b8fd4
Barnut Cristiana <119601150+criss05@users.noreply.github.com>
2025-04-09 00:52:35 +0300

    Refactor WebSocket import to use WebSocketServer for improved functionality

--- src/pages/api/server.js
+++ src/pages/api/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { WebSocket } from "ws";
+import { WebSocketServer } from "ws";
 import cors from "cors";
 import initialHolidays from "../../data/Holidays.js";
 import multer from "multer";
