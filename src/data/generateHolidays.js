3763676999cc370bd3648d6f4ec57fb7bc11f532
Barnut Cristiana <119601150+criss05@users.noreply.github.com>
2025-04-09 00:37:45 +0300

    Refactor holiday generation to use consistent future dates for start and end dates

--- src/data/generateHolidays.js
+++ src/data/generateHolidays.js
@@ -3,12 +3,15 @@ import { faker } from "@faker-js/faker";
 
 
 export const generateRandomHoliday = () => {
+  const startDate = faker.date.future(); // Start date is in the future
+  const endDate = faker.date.between({ from: startDate, to: faker.date.future() });
+
   const holiday = {
     name: faker.commerce.productName(),
     destination: "CLuj-Napoca",
     transport:"Boat",
-    startDate: faker.date.future().toISOString(),
-    endDate: faker.date.past().toISOString(),
+    startDate: startDate.toISOString(),
+    endDate: endDate.toISOString(),
     transport_price: 20,
     accommodation_price: 100
   };
